297a4a265d94f91cdb50ad25b1ec81b7a752e7c2
Seymen Kalkan <seymenkalkan@gmail.com>
2025-01-09 16:01:07 +0300

    Added random maximum commit count to settings

--- lib/settings/include/Settings.hpp
+++ lib/settings/include/Settings.hpp
@@ -11,8 +11,20 @@
  * such as API keys and usernames using QSettings. It ensures that only
  * one instance of the Config class exists throughout the application's lifetime.
  */
-class Settings : public QSettings {
+class Settings final : public QSettings {  // NOLINT
   public:
+    /// Disable copy constructor.
+    Settings(const Settings &) = delete;
+
+    /// Disable assignment operator.
+    Settings &operator=(const Settings &) = delete;
+
+    /// Disable move constructor
+    Settings(const Settings &&) = delete;
+
+    /// Disable move assignment operator
+    Settings &operator=(const Settings &&) = delete;
+
     /**
      * @brief Returns the Singleton instance of the Config class.
      * @return A pointer to the single instance of the Config class.
@@ -23,19 +35,26 @@ class Settings : public QSettings {
      * @brief Retrieves the stored API key.
      * @return The API key as a QString.
      */
-    QString GetAPIKey() const;
+    [[nodiscard]] QString GetAPIKey() const;
 
     /**
      * @brief Retrieves the stored username.
      * @return The username as a QString.
      */
-    QString GetUsername() const;
+    [[nodiscard]] QString GetUsername() const;
 
     /**
      * @brief Retrieves the stored repositories.
      * @return The list of repository paths as a QStringList.
      */
-    QStringList GetRepositories() const;
+    [[nodiscard]] QStringList GetRepositories() const;
+
+    /**
+     * @brief Retrieves the maximum random commit count
+     *
+     * @return quint32 the maximum count
+     */
+    [[nodiscard]] quint32 GetRandomMax() const;
 
     /**
      * @brief Sets the API key and stores it in the settings.
@@ -56,6 +75,13 @@ class Settings : public QSettings {
      */
     void SetRepositories(const QStringList &repos);
 
+    /**
+     * @brief Sets the maximum random commit count and stores it in the settings
+     *
+     * @param count The maximum count
+     */
+    void SetRandomMax(const quint32 &count);
+
   private:
     /**
      * @brief Constructor for the Config class.
@@ -70,20 +96,15 @@ class Settings : public QSettings {
      *
      * Cleans up resources when the Config instance is destroyed.
      */
-    ~Settings();
+    ~Settings() = default;  // NOLINT
 
     /// A pointer to the Singleton instance of the Config class.
-    static Settings *m_Instance;
-
-    /// Disable copy constructor.
-    Settings(const Settings &) = delete;
-
-    /// Disable assignment operator.
-    Settings &operator=(const Settings &) = delete;
+    static Settings *m_Instance;  // NOLINT
 
   private:
     QString m_Key;        ///< Stores the API key.
     QString m_Username;   ///< Stores the username.
+    quint32 m_RandomMax;  ///< Stores the random maximum commit count
     QStringList m_Repos;  ///< Stores the repositories.
 };
 
--- lib/settings/src/Settings.cpp
+++ lib/settings/src/Settings.cpp
@@ -2,11 +2,11 @@
 
 #include "Config.hpp"
 
-Settings *Settings::m_Instance = nullptr;
+Settings *Settings::m_Instance = nullptr;  // NOLINT
 
 Settings *Settings::Instance() {
     if (!m_Instance) {
-        m_Instance = new Settings();
+        m_Instance = new Settings();  // NOLINT
     }
     return m_Instance;
 }
@@ -16,9 +16,7 @@ Settings::Settings()
     m_Key = value("API_KEY", "").toString();
     m_Username = value("USERNAME", "").toString();
     m_Repos = value("REPOSITORIES", QStringList()).toStringList();
-}
-
-Settings::~Settings() {
+    m_RandomMax = value("MAX_RAND_COUNT", quint32()).toUInt();
 }
 
 QString Settings::GetAPIKey() const {
@@ -31,6 +29,10 @@ QString Settings::GetUsername() const {
 
 QStringList Settings::GetRepositories() const {
     return m_Repos;
+}
+
+quint32 Settings::GetRandomMax() const {
+    return m_RandomMax;
 }
 
 void Settings::SetAPIKey(const QString &key) {
@@ -46,4 +48,9 @@ void Settings::SetUsername(const QString &name) {
 void Settings::SetRepositories(const QStringList &repos) {
     m_Repos = repos;
     setValue("REPOSITORIES", m_Repos);
+}
+
+void Settings::SetRandomMax(const quint32 &count) {
+    m_RandomMax = count;
+    setValue("MAX_RAND_COUNT", count);
 }
